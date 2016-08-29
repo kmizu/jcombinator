46c1e479e69f0aa86f0e19e19f27e13c4d071838
Kota Mizushima <mizukota@gmail.com>
2016-08-29 23:50:22 +0900

    Parser#or method is implemented

--- src/main/java/com/github/kmizu/jcombinator/Parser.java
+++ src/main/java/com/github/kmizu/jcombinator/Parser.java
@@ -2,4 +2,7 @@ package com.github.kmizu.jcombinator;
 
 public interface Parser<T> {
 	ParseResult<T> perform(String input);
+	default Parser<T> or(Parser<T> rhs) {
+		return new Or<>(this, rhs);
+	}
 }
