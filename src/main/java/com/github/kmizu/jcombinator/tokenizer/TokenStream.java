496a174fed2289171672c6315bf2bf998f608afd
Kota Mizushima <mizukota@gmail.com>
2016-12-09 16:26:26 +0900

    Remove token-related files

--- src/main/java/com/github/kmizu/jcombinator/tokenizer/TokenStream.java
+++
@@ -1,7 +0,0 @@
-package com.github.kmizu.jcombinator.tokenizer;
-
-public interface TokenStream<T> {
-    T head();
-    TokenStream<T> tail();
-    public boolean isTerminal();
-}
--- src/test/java/com/github/kmizu/jcombinator/AllTests.java
+++ src/test/java/com/github/kmizu/jcombinator/AllTests.java
@@ -4,6 +4,6 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({PrimitiveTest.class, ChainlTest.class, TokenStreamTest.class})
+@Suite.SuiteClasses({PrimitiveTest.class, ChainlTest.class})
 public class AllTests {
 }
