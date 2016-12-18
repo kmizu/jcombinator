f55238f361b156a3d855f0ec194cecd16448bd82
Kota Mizushima <mizukota@gmail.com>
2016-12-18 22:57:41 +0900

    RegexInterpreter is implemented

--- src/main/java/com/github/kmizu/jcombinator/example/regex/RegexInterpreter.java
+++ src/main/java/com/github/kmizu/jcombinator/example/regex/RegexInterpreter.java
@@ -1,44 +1,141 @@
 package com.github.kmizu.jcombinator.example.regex;
 
+import com.github.kmizu.jcombinator.ParseResult;
+import com.github.kmizu.jcombinator.datatype.Function0;
+import com.github.kmizu.jcombinator.datatype.Function1;
+
 public class RegexInterpreter {
-    private static class InterpreterContext {
-        String input;
-        InterpreterContext(String input) {
-            this.input = input;
+    interface SuccessfullContinuation extends Function0<Boolean> {}
+    private static class ExpressionRewriter implements AstNode.RVisitor<AstNode.RExpression, Void> {
+        @Override
+        public AstNode.RExpression visitRGrouped(AstNode.RGrouped node, Void context) {
+            return new AstNode.RGrouped(node.getTarget().accept(this, context));
         }
-        public void advance(int length) {
-            this.input = input.substring(length);
+
+        @Override
+        public AstNode.RExpression visitRString(AstNode.RString node, Void context) {
+            return node;
+        }
+
+        @Override
+        public AstNode.RExpression visitRChoice(AstNode.RChoice node, Void context) {
+            return new AstNode.RChoice(
+                node.getLhs().accept(this, context),
+                node.getRhs().accept(this, context)
+            );
+        }
+
+        @Override
+        public AstNode.RExpression visitRSequence(AstNode.RSequence node, Void context) {
+            return new AstNode.RSequence(
+                node.getLhs().accept(this, context),
+                node.getRhs().accept(this, context)
+            );
+        }
+
+        @Override
+        public AstNode.RExpression visitRRepeat0(AstNode.RRepeat0 node, Void context) {
+            return new AstNode.RRepeat0(
+                node.getTarget().accept(this, context)
+            );
+        }
+
+        @Override
+        public AstNode.RExpression visitRRepeat1(AstNode.RRepeat1 node, Void context) {
+            AstNode.RExpression target = node.getTarget().accept(this, context);
+            return new AstNode.RSequence(
+                target,
+                new AstNode.RRepeat0(target)
+            );
         }
     }
-    private static class ExpressionVisitor implements  AstNode.RVisitor<Lazy<String>, InterpreterContext> {
-        @Override
-        public Lazy<String> visitRGrouped(AstNode.RGrouped node, InterpreterContext context) {
-            return null;
+    private static class ExpressionInterpreter implements  AstNode.RVisitor<Boolean, SuccessfullContinuation> {
+        private String input;
+        private int cursor;
+
+        public ExpressionInterpreter(String input) {
+            this.input = input;
+            this.cursor = cursor;
         }
 
         @Override
-        public Lazy<String> visitRString(AstNode.RString node, InterpreterContext context) {
-            return null;
+        public Boolean visitRGrouped(AstNode.RGrouped node, SuccessfullContinuation context) {
+            return node.getTarget().accept(this, context);
         }
 
         @Override
-        public Lazy<String> visitRChoice(AstNode.RChoice node, InterpreterContext context) {
-            return null;
+        public Boolean visitRString(AstNode.RString node, SuccessfullContinuation context) {
+            String slice = input.substring(cursor);
+            if(slice.startsWith(node.getLiteral())) {
+                cursor += node.getLiteral().length();
+                return context.invoke();
+            } else {
+                return false;
+            }
         }
 
         @Override
-        public Lazy<String> visitRSequence(AstNode.RSequence node, InterpreterContext context) {
-            return null;
+        public Boolean visitRChoice(AstNode.RChoice node, SuccessfullContinuation context) {
+            final int start = cursor;
+            final Function0<Boolean> onFailAlt = () -> {
+                cursor = start;
+                return node.getRhs().accept(this, context);
+            };
+            final SuccessfullContinuation onSuccess = () -> {
+                if(context.invoke()) {
+                    return true;
+                } else {
+                    return onFailAlt.invoke();
+                }
+            };
+            return node.getLhs().accept(this, onSuccess);
         }
 
         @Override
-        public Lazy<String> visitRRepeat0(AstNode.RRepeat0 node, InterpreterContext context) {
-            return null;
+        public Boolean visitRSequence(AstNode.RSequence node, SuccessfullContinuation context) {
+            return node.getLhs().accept(this, () ->
+                node.getRhs().accept(this, context)
+            );
         }
 
         @Override
-        public Lazy<String> visitRRepeat1(AstNode.RRepeat1 node, InterpreterContext context) {
-            return null;
+        public Boolean visitRRepeat0(AstNode.RRepeat0 node, SuccessfullContinuation context) {
+            Function1<Function0<Boolean>, Boolean>[] onSuccessRep = new Function1[1];
+            onSuccessRep[0] = (f) -> {
+                final int start = cursor;
+                final SuccessfullContinuation nf = () -> {
+                    cursor = start;
+                    return context.invoke() ? true : f.invoke();
+                };
+                return (node.getTarget().accept(
+                    this,
+                    () -> onSuccessRep[0].invoke(nf)
+                ) ? true : nf.invoke());
+            };
+            return onSuccessRep[0].invoke(context);
         }
+
+        @Override
+        public Boolean visitRRepeat1(AstNode.RRepeat1 node, SuccessfullContinuation context) {
+            throw new UnsupportedOperationException("ExpressionInterpreter#visit(AstNode.RRepeat1, SuccessfullContinuation)");
+        }
+    }
+
+    public boolean matches(String regex, String input) {
+        ParseResult<AstNode.RExpression> result = new RegexParser().expression().invoke(regex);
+        boolean[] matchResult = new boolean[1];
+        matchResult[0] = false;
+        result.onSuccess(e -> {
+            ExpressionRewriter rewriter = new ExpressionRewriter();
+            AstNode.RExpression newE = e.value().accept(rewriter, null);
+            ExpressionInterpreter interpreter = new ExpressionInterpreter(input);
+            matchResult[0] = newE.accept(interpreter, () -> true);
+        });
+        return matchResult[0];
+    }
+
+    public static void main(String[] args) {
+        RegexInterpreter i = new RegexInterpreter();
+        System.out.println(i.matches("a*aaaa", "aaa"));
     }
 }
